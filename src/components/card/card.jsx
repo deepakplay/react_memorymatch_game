6a76ec10b16cdb42daf6fbb9fef65c2d6132e3f3
Deepak Play <deepakplay14@gmail.com>
2022-05-01 17:03:27 +0530

    Game setup

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './card.scss';
 import front from '../../assets/cover.png';
 import back from '../../assets/helmet-1.png';
 
 export default function Card() {
+    const [flipped, setFlipped] = useState(false);
     return (
-        <div className='card'>
-            <div className='card-image-container'>
+        <div className='card' onClick={()=> setFlipped(!flipped)} >
+            <div className={`card-image-container${flipped?' flipped':''}`}>
                 <div className="card-image front" style={{ background: `url(${front})` }}></div>
                 <div className="card-image back" style={{ background: `url(${back})` }}></div>
             </div>
--- src/components/cardlist/cardlist.jsx
+++ src/components/cardlist/cardlist.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import './cardlist.scss';
+import './gamedata.scss';
 import Card from '../card/card';
 
 export default function Cardlist() {
     return (
-        <div className='CardList'>
-            { 
-                [...Array(12)].map(()=> <Card/>)
-            }
-        </div>
+        <>
+            <div className='CardList'>
+                {
+                    [...Array(12)].map(() => <Card />)
+                }
+            </div>
+            <div className='Game_data'>
+                <span className='turn_count'>Turns : {0}</span>                
+            </div>
+        </>
     );
 }
