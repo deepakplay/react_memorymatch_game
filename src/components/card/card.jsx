d41e99f37c50411a93be43569bb7ffc0c042cd65
Deepak Play <deepakplay14@gmail.com>
2022-05-02 08:30:52 +0530

    css effect

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './card.scss';
 import { CARD_BACK as back } from '../../data/carddata';
 
-export default function Card({ front, isflipped, handleClick, id }) {
+export default function Card({ front, isflipped, handleClick, id, isOpened }) {
 
     return (
         <div className='card' onClick={() => handleClick(id)} >            
-            <div className={`card-image-container${isflipped ? ' flipped' : ''}`}>
+            <div className={`card-image-container${isflipped ? ' flipped' : ''}${isOpened ? ' opened' : ''}`}>
                 <div className="card-image front" style={{ background: `url(${front})` }}></div>
                 <div className="card-image back" style={{ background: `url(${back})` }}></div>
             </div>
--- src/components/cardlist/cardlist.jsx
+++ src/components/cardlist/cardlist.jsx
@@ -1,57 +1,65 @@
-import React, { useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import './cardlist.scss';
 import './gamedata.scss';
 import Card from '../card/card';
 import { useCardListContext } from '../../context/cardlist';
 
 export default function Cardlist() {
-    const {cardList, setCardList, turns, setTurns} = useCardListContext();    
-    const [turnCount, setTurnCount] = useState(0);    
+    const { cardList, setCardList, turns, setTurns, newgame } = useCardListContext();
+    const [turnCount, setTurnCount] = useState(0);
+
+
 
     useEffect(() => {
-        if(!cardList) return;
+        if (!cardList) return;
+
         if (turnCount === 2) {
             const matchedCardList = cardList.filter((card) => card.isflipped && !card.isOpened);
 
             let isMatch = false;
-            if(matchedCardList.length===2){
+            if (matchedCardList.length === 2) {
                 isMatch = matchedCardList[0].front === matchedCardList[1].front;
             }
 
-            if(isMatch){
+            if (isMatch) {
                 const newCardList = cardList.map((card) => {
-                    return (card.isflipped && !card.isOpened ) ? { ...card, isOpened: true} : card;
+                    return (card.isflipped && !card.isOpened) ? { ...card, isOpened: true } : card;
                 });
                 setCardList(newCardList);
                 setTurnCount(0);
-            }else{
+                setTimeout(() => {
+                    if (!newCardList.some((card) => !card.isOpened) && window.confirm("You Win!!, Do you like to restart the game?")) {
+                        newgame();
+                    }
+                }, 1000)
+            } else {
                 setTimeout(() => {
                     const newCardList = cardList.map((card) => {
                         return (!card.isOpened) ? { ...card, isflipped: false } : card;
                     });
                     setCardList(newCardList);
                     setTurnCount(0);
-                }, 1000);                
-            }            
+                }, 1000);
+            }
         }
-    }, [turnCount, cardList, setCardList, setTurnCount])
+    }, [turnCount, cardList, setCardList, setTurnCount, newgame])
 
     const handleClick = (id) => {
         if (turnCount >= 2) return;
-        if(cardList.find((card)=>card.id===id).isOpened) return;
+        if (cardList.find((card) => card.id === id).isOpened) return;
         setTurnCount((preCount) => preCount + 1);
 
         const newCardList = cardList.map((card) => {
             return (card.id === id) ? { ...card, isflipped: true } : card
         });
         setCardList(newCardList);
-        setTurns((preTurns) => preTurns + 1);        
+        setTurns((preTurns) => preTurns + 1);
     };
 
     return (
         <>
             <div className='CardList'>
-                {                    
+                {
                     cardList && cardList.map((card) => {
                         return <Card key={card.id} {...card} handleClick={handleClick} />;
                     })
